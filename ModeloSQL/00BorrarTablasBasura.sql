a163487b50384e699af28cc95a22c2f259fce28e
takeshidev <59844178+takeshidev@users.noreply.github.com>
2020-07-01 21:53:16 -0400

    ahora si que si

--- ModeloSQL/00BorrarTablasBasura.sql
+++ ModeloSQL/00BorrarTablasBasura.sql
@@ -3,6 +3,8 @@ PARA BORRAR DATOS AUTO GENERADOS POR EL ORACLE
 1. Ejecutar query
 2. Copiar resultado y ejecutarlo
 */
-
-
 SELECT 'DROP TABLE "' || TABLE_NAME || '" CASCADE CONSTRAINTS;' FROM user_tables;
+select 'drop sequence ' || sequence_name || ';' from user_sequences;
+ALTER SESSION SET PLSCOPE_SETTINGS = 'IDENTIFIERS:NONE';
+
+
